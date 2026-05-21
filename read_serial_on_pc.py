e20034d2a95e1fcb8188d55112efda54306779ff
plsatin <plsatin@yandex.ru>
2026-05-21 23:55:33 +0500

    Гладкий график (не подходит)

--- read_serial_on_pc.py
+++ read_serial_on_pc.py
@@ -7,6 +7,14 @@ import subprocess
 import sys
 
 
+## Гладкий график
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+from scipy.interpolate import make_interp_spline
+
+
+
 # Пример данных: время и температура
 # Время (например, часы)
 time_list = []
@@ -17,13 +25,22 @@ error_count = 0
 temp_color = 'green'
 temp_critical = 27.0
 
+temperature.append(24.0)
+time_list.append(2)
+temperature.append(24.1)
+time_list.append(3)
+
+
 
 while True:
 
     counter = counter + 10
-    time_list.append(counter)
     temp_from_pico = 0.0
     temp_from_pico_prev = 0.0
+
+    ## Добавляем пустые данные для гладкого графика
+    time_list.append(counter)
+
 
     try:
         temp_from_pico = subprocess.run([sys.executable, "host_command.py", "temperature"], capture_output=True, text=True).stdout.strip()
@@ -43,8 +60,24 @@ while True:
     else:
         temp_color = 'green'
 
-    # Создание графика
-    plt.plot(time_list, temperature, color=temp_color)
+    
+    x = np.array(time_list)
+    y = np.array(temperature)
+
+    # Параметр s отвечает за сглаживание: чем больше число, тем более гладкой будет линия
+    spl = UnivariateSpline(x, y, k=2, s=1) 
+    # # Автоматически строит плавную линию через все точки
+    # spl = make_interp_spline(x, y) 
+
+    x_new = np.linspace(x.min(), x.max(), 300)
+    y_new = spl(x_new)
+
+    plt.clf()
+    plt.plot(x_new, y_new, color=temp_color)
+
+
+    # # Создание графика
+    # plt.plot(time_list, temperature, color=temp_color)
 
     # Настройка заголовка и меток осей
     plt.title('График температуры')
@@ -53,22 +86,10 @@ while True:
 
     plt.savefig("temperature_plot.png", dpi=180)
 
-    # # Отображение графика
-    # # plt.show()
-
-    # ## plt.show - блокирует выполнение программы поэтому используем pause - показывая график на 10 сек.
-    # plt.pause(10)
-    # plt.close()
-
-
     time.sleep(10.0)
 
     # Держим график в диапозоне 600 секунд
     if counter > 600:
-        # time_list = []
-        # counter = 0
-        # temperature = []
-
         # Удаляем устаревший элемент
         del time_list[0]
         del temperature[0]
